e13b434822bf7c019293e8a74af16289e5ec8db0
Michael Van Brummen <michaelvanbrummen@gmail.com>
2022-01-01 15:59:27 +1000

    Refactoring to use delegate

--- src/main/kotlin/com/mvanbrummen/emailservicekotlin/service/EmailService.kt
+++ src/main/kotlin/com/mvanbrummen/emailservicekotlin/service/EmailService.kt
@@ -1,11 +1,7 @@
 package com.mvanbrummen.emailservicekotlin.service
 
-import com.mvanbrummen.emailservicekotlin.api.EmailSendRequest
 import com.mvanbrummen.emailservicekotlin.gateway.EmailGateway
 import org.springframework.stereotype.Service
 
 @Service
-class EmailService(private val emailGateway: EmailGateway) {
-
-    fun sendEmail(emailSendRequest: EmailSendRequest) = emailGateway.sendEmail(emailSendRequest)
-}
+class EmailService(private val emailGateway: EmailGateway) : EmailGateway by emailGateway
